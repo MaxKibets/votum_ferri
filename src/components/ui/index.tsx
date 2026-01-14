0ab4ad0f6cff121d4ce0b14f863af4e9c32d12e0
MaxKibets <kibetss3@gmail.com>
2026-01-14 12:38:27 +0300

    feat(ui): integrate shadcn ui components and utilities

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,57 @@
+// shadcn/ui components exports
+export { Button, buttonVariants } from "./button";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./card";
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from "./dialog";
+export {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "./form";
+export { Input } from "./input";
+export { Label } from "./label";
+export { Calendar } from "./calendar";
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+} from "./table";
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from "./select";
+export { Badge, badgeVariants } from "./badge";
